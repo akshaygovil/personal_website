e2a38c290f7741ddb46db1775740360756798c61
Akshay Govil <akshaygovil@Akshays-MacBook-Pro.local>
2026-03-29 21:17:43 +1100

    your message here

--- src/app/photography/page.tsx
+++ src/app/photography/page.tsx
@@ -2,5 +2,10 @@ import { photography } from "../lib/photography";
 import GalleryClient from "./GalleryClient";
 
 export default function Page() {
+    return null;
+}
+/*
+export default function Page() {
   return <GalleryClient photography={photography} />;
 }
+*/
